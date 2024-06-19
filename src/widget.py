3ef4106e8aaf70eb187770a3bc2200c865b47ad9
Galina Lukina <lukina.galina11@gmail.com>
2024-06-19 14:10:42 +0400

    добалена функция get_data

--- src/widget.py
+++ src/widget.py
@@ -2,11 +2,17 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_or_account_info: str) -> str:
-    '''Возвращает маску счёта или карты, исходя из полученных данных'''
+    """Возвращает маску счёта или карты, исходя из полученных данных"""
+
     digits = card_or_account_info.split()[-1]
 
     if card_or_account_info.lower().startswith("счет"):
-
         return get_mask_account(digits)
 
     return get_mask_card_number(digits)
+
+
+def get_data(date_info: str) -> str:
+    """Возвращает дату в формате ДД.ММ.ГГГГ"""
+
+    return f"{date_info[8:10]}.{date_info[5:7]}.{date_info[:4]}"
