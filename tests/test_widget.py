f7ca805378b947f35b9dd2ff4339482c5d874a0f
Galina Lukina <lukina.galina11@gmail.com>
2024-07-01 20:59:06 +0400

    add tests, fix get_card_number and get_mask_account

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,12 @@
-from src.widget import mask_account_card, get_data
 import pytest
 
+from src.widget import get_data, mask_account_card
 
-@pytest.mark.parametrize("card_or_account_info, expected", [("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"), ("Счет 73654108430135874305", "Счет **4305")])
+
+@pytest.mark.parametrize("card_or_account_info, expected",
+                         [("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+                          ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                          ("Счет 73654108430135874305", "Счет **4305")])
 def test_mask_account_card(card_or_account_info, expected):
     assert mask_account_card(card_or_account_info) == expected
 
