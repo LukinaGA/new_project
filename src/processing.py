16b0d7a99f42e3c27bc7ec85f01088021a27d007
Galina Lukina <lukina.galina11@gmail.com>
2024-07-01 20:23:29 +0400

    add test_processing, fix sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -4,8 +4,10 @@ def filter_by_state(transaction_info: list[dict], state: str = "EXECUTED") -> li
     return [transaction for transaction in transaction_info if transaction["state"] == state]
 
 
-def sort_by_date(transaction_info: list[dict], is_decreasing : bool = True) -> list[dict]:
+def sort_by_date(transaction_info: list[dict], is_decreasing: bool = True) -> list[dict]:
     """Сортирует список транзакций по дате"""
+    if len(transaction_info) == 1:
+        return transaction_info
 
     return sorted(
         transaction_info, key=lambda transaction: transaction["date"][:10].replace("-", ""), reverse=is_decreasing
