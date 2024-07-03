1d8c5b28dc6803ca362d37dac01c9bc006b538e8
Galina Lukina <lukina.galina11@gmail.com>
2024-07-03 15:49:08 +0400

    add fixtures in conftest for processing and generators

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,25 +1,13 @@
-import pytest
-
 from src.processing import filter_by_state, sort_by_date
 
 
-@pytest.fixture
-def transaction_info_for_test():
-    return [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]
-
-
-def test_filter_by_state(transaction_info_for_test):
-    assert filter_by_state(transaction_info_for_test) == [
+def test_filter_by_state(transaction_info_for_processing):
+    assert filter_by_state(transaction_info_for_processing) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-    assert filter_by_state(transaction_info_for_test, "CANCELED") == [
+    assert filter_by_state(transaction_info_for_processing, "CANCELED") == [
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     ]
@@ -31,25 +19,25 @@ def test_filter_by_state_empty_list():
 
 def test_filter_by_state_without_required_state():
     assert (
-            filter_by_state(
-                [
-                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                ]
-            )
-            == []
+        filter_by_state(
+            [
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            ]
+        )
+        == []
     )
 
 
-def test_sort_by_data(transaction_info_for_test):
-    assert sort_by_date(transaction_info_for_test) == [
+def test_sort_by_data(transaction_info_for_processing):
+    assert sort_by_date(transaction_info_for_processing) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-    assert sort_by_date(transaction_info_for_test, False) == [
+    assert sort_by_date(transaction_info_for_processing, False) == [
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
@@ -62,8 +50,10 @@ def test_sort_by_date_empty_list():
 
 
 def test_sort_by_date_one_transaction():
-    assert sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]) == [{
-        "id": 41428829,
-        "state": "EXECUTED",
-        "date": "2019-07-03T18:35:29.512364",
-    }]
+    assert sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]) == [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+        }
+    ]
