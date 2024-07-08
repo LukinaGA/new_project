963023746a31343a2aca1be6842b736e24b7b635
Galina Lukina <lukina.galina11@gmail.com>
2024-07-08 13:08:23 +0400

    add file with decorator "log"

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+
+def log(filename):
+    """Логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки"""
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+            except Exception as error:
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} error: {error}. Inputs {args}, {kwargs})")
+                else:
+                    print(f"{func.__name__} error: {error}. Inputs {args}, {kwargs})")
+            else:
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"{func.__name__} ok. Result: {result}")
+                else:
+                    print(f"{func.__name__} ok. Result: {result}")
+
+        return wrapper
+
+    return decorator
