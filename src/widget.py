974cbc9ca72ffd80210a5d2c08054297c0158c12
Galina Lukina <lukina.galina11@gmail.com>
2024-07-30 16:29:47 +0400

    rewrite mask_account_card()

--- src/widget.py
+++ src/widget.py
@@ -3,18 +3,21 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_info: str) -> str:
     """Возвращает маску счёта или карты, исходя из полученных данных"""
-    card_or_acc_info_list = card_or_account_info.split()
-    words = card_or_acc_info_list[:-1]
-    digits = card_or_acc_info_list[-1]
+    if card_or_account_info:
+        card_or_acc_info_list = card_or_account_info.split()
+        words = card_or_acc_info_list[:-1]
+        digits = card_or_acc_info_list[-1]
 
-    if card_or_account_info.lower().startswith("счет"):
+        if card_or_account_info.lower().startswith("счет"):
 
-        return f"{" ".join(words)} {get_mask_account(digits)}"
+            return f"{" ".join(words)} {get_mask_account(digits)}"
 
-    return f"{" ".join(words)} {get_mask_card_number(digits)}"
+        return f"{" ".join(words)} {get_mask_card_number(digits)}"
+
+    return ""
 
 
-def get_data(date_info: str) -> str:
+def get_date(date_info: str) -> str:
     """Возвращает дату в формате ДД.ММ.ГГГГ"""
 
     return f"{date_info[8:10]}.{date_info[5:7]}.{date_info[:4]}"
